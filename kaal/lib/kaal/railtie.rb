83cd29db57fec80b9d56f63562a382fe7685a837
Nitesh Purohit <nitesh.purohit.it@gmail.com>
2026-03-12 21:00:05 -0400

    refactor: update copyright information and links
    # Description
    This commit updates the copyright information in the footer and adds the 'rel="noopener noreferrer"' attribute to the API documentation link for improved security.
    
    # Related Issue
    N/A
    
    # Proposed Changes
    - Corrected the copyright holder name from "Code Vedas Inc." to "Codevedas Inc." for consistency.
    - Added 'rel="noopener noreferrer"' to the API documentation link to enhance security against reverse tabnabbing.
    
    # Screenshots or Additional Information
    N/A

--- kaal/lib/kaal/railtie.rb
+++ kaal/lib/kaal/railtie.rb
@@ -7,11 +7,6 @@
 
 require 'pathname'
 
-# Copyright Codevedas Inc. 2025-present
-#
-# This source code is licensed under the MIT license found in the
-# LICENSE file in the root directory of this source tree.
-
 module Kaal
   ##
   # Railtie class to integrate Kaal with Rails applications.
